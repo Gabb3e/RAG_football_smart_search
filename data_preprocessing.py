f5a098e291cd9f2c07703fea339248a212226b71
Gabb3e <gabrielchandia@hotmail.com>
2024-10-18 00:32:02 +0200

    Cleaned the df by removing urls and links.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,7 +5,11 @@ file_path = 'players.csv'
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(file_path)
+df_cleaned = df.drop(columns=['image_url', 'url'])
 
 # Print the first few rows of the DataFrame to check the data
 #print(df.head())
-print(df.columns)
+#print(df_cleaned.head())
+print(df_cleaned.columns)
+print(df_cleaned.shape)
+#print(df_cleaned.info())
--- indexing.py
+++ indexing.py
@@ -1,7 +1,7 @@
 import os
 from whoosh.index import create_in
 from whoosh.fields import Schema, TEXT, ID
-from data_preprocessing import df
+from data_preprocessing import df_cleaned
 
 # Define schema (fields to be indexed)
 schema = Schema(player_id=ID(stored=True), name=TEXT(stored=True), content=TEXT(stored=True))
@@ -18,7 +18,7 @@ if not os.path.exists(os.path.join(index_dir, 'index')):
     ix = create_in(index_dir, schema)
     writer = ix.writer()
     
-    for index, row in df.iterrows():
+    for index, row in df_cleaned.iterrows():
         full_name = f"{row['first_name']} {row['last_name']}"
         content = f"Player ID: {row['player_id']}, Name: {full_name}, " \
               f"Last Season: {row['last_season']}, Current Club ID: {row['current_club_id']}, " \
@@ -26,7 +26,7 @@ if not os.path.exists(os.path.join(index_dir, 'index')):
               f"Country of Citizenship: {row['country_of_citizenship']}, Date of Birth: {row['date_of_birth']}, " \
               f"Sub Position: {row['sub_position']}, Position: {row['position']}, Foot: {row['foot']}, " \
               f"Height: {row['height_in_cm']} cm, Contract Expiration: {row['contract_expiration_date']}, " \
-              f"Agent: {row['agent_name']}, Image URL: {row['image_url']}, Profile URL: {row['url']}, " \
+              f"Agent: {row['agent_name']}, " \
               f"Domestic Competition ID: {row['current_club_domestic_competition_id']}, " \
               f"Club: {row['current_club_name']}, Market Value: {row['market_value_in_eur']} EUR, " \
               f"Highest Market Value: {row['highest_market_value_in_eur']} EUR."
